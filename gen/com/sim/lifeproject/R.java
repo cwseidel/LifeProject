8f4b98a094911280d702de1bd3076d7c145415e3
ferriman <ferri.fc@gmail.com>
2012-12-18 15:08:32 +0100

    android 4 styles if available.

--- gen/com/sim/lifeproject/R.java
+++ gen/com/sim/lifeproject/R.java
@@ -21,71 +21,71 @@ public final class R {
         public static final int starttablet=0x7f020007;
     }
     public static final class id {
-        public static final int LinearLayout02=0x7f060002;
-        public static final int LinearLayout03=0x7f060006;
-        public static final int graficsimulacio=0x7f060005;
-        public static final int grow_edit1=0x7f06000b;
-        public static final int grow_edit2=0x7f06000d;
-        public static final int grow_txt1=0x7f06000a;
-        public static final int grow_txt2=0x7f06000c;
-        public static final int help=0x7f06003d;
-        public static final int insidetab2=0x7f060009;
-        public static final int insidetab3=0x7f060016;
-        public static final int insidetab4=0x7f060029;
-        public static final int insidetab5=0x7f06003c;
-        public static final int menu_exit=0x7f06003e;
-        public static final int menu_start=0x7f060040;
-        public static final int menu_stop=0x7f06003f;
-        public static final int pantallasimulacio=0x7f060001;
-        public static final int plants_update=0x7f060014;
-        public static final int rain_edit1=0x7f06000f;
-        public static final int rain_edit2=0x7f060011;
-        public static final int rain_edit3=0x7f060013;
-        public static final int rain_txt1=0x7f06000e;
-        public static final int rain_txt2=0x7f060010;
-        public static final int rain_txt3=0x7f060012;
-        public static final int show_info=0x7f060003;
-        public static final int show_infograph=0x7f060007;
-        public static final int specie1_edit1=0x7f060018;
-        public static final int specie1_edit2=0x7f06001a;
-        public static final int specie1_edit3=0x7f06001c;
-        public static final int specie1_edit4=0x7f060022;
-        public static final int specie1_edit5=0x7f060024;
-        public static final int specie1_edit6=0x7f06001e;
-        public static final int specie1_edit7=0x7f060020;
-        public static final int specie1_edit8=0x7f060026;
-        public static final int specie1_txt1=0x7f060017;
-        public static final int specie1_txt2=0x7f060019;
-        public static final int specie1_txt3=0x7f06001b;
-        public static final int specie1_txt4=0x7f060021;
-        public static final int specie1_txt5=0x7f060023;
-        public static final int specie1_txt6=0x7f06001d;
-        public static final int specie1_txt7=0x7f06001f;
-        public static final int specie1_txt8=0x7f060025;
-        public static final int specie1_update=0x7f060027;
-        public static final int specie2_edit1=0x7f06002b;
-        public static final int specie2_edit2=0x7f06002d;
-        public static final int specie2_edit3=0x7f06002f;
-        public static final int specie2_edit4=0x7f060035;
-        public static final int specie2_edit5=0x7f060037;
-        public static final int specie2_edit6=0x7f060031;
-        public static final int specie2_edit7=0x7f060033;
-        public static final int specie2_edit8=0x7f060039;
-        public static final int specie2_txt1=0x7f06002a;
-        public static final int specie2_txt2=0x7f06002c;
-        public static final int specie2_txt3=0x7f06002e;
-        public static final int specie2_txt4=0x7f060034;
-        public static final int specie2_txt5=0x7f060036;
-        public static final int specie2_txt6=0x7f060030;
-        public static final int specie2_txt7=0x7f060032;
-        public static final int specie2_txt8=0x7f060038;
-        public static final int specie2_update=0x7f06003a;
-        public static final int tab1=0x7f060000;
-        public static final int tab2=0x7f060008;
-        public static final int tab3=0x7f060015;
-        public static final int tab4=0x7f060028;
-        public static final int tab5=0x7f06003b;
-        public static final int tab6=0x7f060004;
+        public static final int LinearLayout02=0x7f070002;
+        public static final int LinearLayout03=0x7f070006;
+        public static final int graficsimulacio=0x7f070005;
+        public static final int grow_edit1=0x7f07000b;
+        public static final int grow_edit2=0x7f07000d;
+        public static final int grow_txt1=0x7f07000a;
+        public static final int grow_txt2=0x7f07000c;
+        public static final int help=0x7f07003d;
+        public static final int insidetab2=0x7f070009;
+        public static final int insidetab3=0x7f070016;
+        public static final int insidetab4=0x7f070029;
+        public static final int insidetab5=0x7f07003c;
+        public static final int menu_exit=0x7f07003e;
+        public static final int menu_start=0x7f070040;
+        public static final int menu_stop=0x7f07003f;
+        public static final int pantallasimulacio=0x7f070001;
+        public static final int plants_update=0x7f070014;
+        public static final int rain_edit1=0x7f07000f;
+        public static final int rain_edit2=0x7f070011;
+        public static final int rain_edit3=0x7f070013;
+        public static final int rain_txt1=0x7f07000e;
+        public static final int rain_txt2=0x7f070010;
+        public static final int rain_txt3=0x7f070012;
+        public static final int show_info=0x7f070003;
+        public static final int show_infograph=0x7f070007;
+        public static final int specie1_edit1=0x7f070018;
+        public static final int specie1_edit2=0x7f07001a;
+        public static final int specie1_edit3=0x7f07001c;
+        public static final int specie1_edit4=0x7f070022;
+        public static final int specie1_edit5=0x7f070024;
+        public static final int specie1_edit6=0x7f07001e;
+        public static final int specie1_edit7=0x7f070020;
+        public static final int specie1_edit8=0x7f070026;
+        public static final int specie1_txt1=0x7f070017;
+        public static final int specie1_txt2=0x7f070019;
+        public static final int specie1_txt3=0x7f07001b;
+        public static final int specie1_txt4=0x7f070021;
+        public static final int specie1_txt5=0x7f070023;
+        public static final int specie1_txt6=0x7f07001d;
+        public static final int specie1_txt7=0x7f07001f;
+        public static final int specie1_txt8=0x7f070025;
+        public static final int specie1_update=0x7f070027;
+        public static final int specie2_edit1=0x7f07002b;
+        public static final int specie2_edit2=0x7f07002d;
+        public static final int specie2_edit3=0x7f07002f;
+        public static final int specie2_edit4=0x7f070035;
+        public static final int specie2_edit5=0x7f070037;
+        public static final int specie2_edit6=0x7f070031;
+        public static final int specie2_edit7=0x7f070033;
+        public static final int specie2_edit8=0x7f070039;
+        public static final int specie2_txt1=0x7f07002a;
+        public static final int specie2_txt2=0x7f07002c;
+        public static final int specie2_txt3=0x7f07002e;
+        public static final int specie2_txt4=0x7f070034;
+        public static final int specie2_txt5=0x7f070036;
+        public static final int specie2_txt6=0x7f070030;
+        public static final int specie2_txt7=0x7f070032;
+        public static final int specie2_txt8=0x7f070038;
+        public static final int specie2_update=0x7f07003a;
+        public static final int tab1=0x7f070000;
+        public static final int tab2=0x7f070008;
+        public static final int tab3=0x7f070015;
+        public static final int tab4=0x7f070028;
+        public static final int tab5=0x7f07003b;
+        public static final int tab6=0x7f070004;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -93,7 +93,7 @@ public final class R {
         public static final int splash=0x7f030002;
     }
     public static final class menu {
-        public static final int menu=0x7f050000;
+        public static final int menu=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
@@ -125,5 +125,33 @@ public final class R {
         public static final int specie_txt8=0x7f04000e;
         public static final int specie_txt8_hint=0x7f04001b;
         public static final int update=0x7f040001;
+    }
+    public static final class style {
+        /** 
+        Base application theme, dependent on API level. This theme is replaced
+        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+    
+
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
+
+        Base application theme for API 11+. This theme completely replaces
+        AppBaseTheme from res/values/styles.xml on API 11+ devices.
+    
+ API 11 theme customizations can go here. 
+
+        Base application theme for API 14+. This theme completely replaces
+        AppBaseTheme from BOTH res/values/styles.xml and
+        res/values-v11/styles.xml on API 14+ devices.
+    
+ API 14 theme customizations can go here. 
+         */
+        public static final int AppBaseTheme=0x7f050000;
+        /**  Application theme. 
+ All customizations that are NOT specific to a particular API-level can go here. 
+         */
+        public static final int AppTheme=0x7f050001;
     }
 }
